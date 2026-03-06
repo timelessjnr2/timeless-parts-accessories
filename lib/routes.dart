d8d4ab1d0952532d95481364e71c4e2a8de7b3ef
timelessjnr2 <shawnanthonythomos@gmail.com>
2026-03-05 20:31:29 -0500

    Update to import AppDrawer and use it on screens

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,68 +1,15 @@
 import 'package:flutter/material.dart';
+import 'widgets/app_drawer.dart';
 
-class Routes {
-  static Map<String, WidgetBuilder> get routes {
-    return {
-      '/inventory': (context) => InventoryScreen(),
-      '/vehicles': (context) => VehiclesScreen(),
-      '/invoices': (context) => InvoicesScreen(),
-      '/customers': (context) => CustomersScreen(),
-      '/settings': (context) => SettingsScreen(),
-    };
-  }
-}
-
-class InventoryScreen extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Inventory')), 
-      drawer: Drawer(),
-      body: Center(child: Text('Inventory Screen')), 
+      drawer: const AppDrawer(),
+      appBar: AppBar(title: Text('Home')), // Example
+      body: Center(child: Text('Welcome to your app!')), // Example
     );
   }
 }
 
-class VehiclesScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Vehicles')), 
-      drawer: Drawer(),
-      body: Center(child: Text('Vehicles Screen')), 
-    );
-  }
-}
-
-class InvoicesScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Invoices')), 
-      drawer: Drawer(),
-      body: Center(child: Text('Invoices Screen')), 
-    );
-  }
-}
-
-class CustomersScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Customers')), 
-      drawer: Drawer(),
-      body: Center(child: Text('Customers Screen')), 
-    );
-  }
-}
-
-class SettingsScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Settings')), 
-      drawer: Drawer(),
-      body: Center(child: Text('Settings Screen')), 
-    );
-  }
-}
+// Repeat for other screens as necessary
